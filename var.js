26178fb1b1c137ea04945a5c9a884cd7be8702c1
Shruti Rouniyar <shrutirouniyar06@gmail.com>
2025-07-20 22:30:09 +0530

    Variables and values in JS

---
+++ var.js
@@ -0,0 +1,11 @@
+//Here, we've mentioend the concepts of variables and values.
+var myAge = 22;
+console.log(myAge); // This will print 22 to the console
+
+var myNum = "10";
+console.log(typeof myNum); // This will print "10" to the console
+console.log( typeof +myNum);
+
+var strNum = 5;
+console.log(typeof String(strNum));
+console.log( typeof strNum+ "");
