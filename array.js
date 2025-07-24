6725288ec73477deecf58048a90909c036e12e3b
Shruti Rouniyar <shrutirouniyar06@gmail.com>
2025-07-24 23:16:40 +0530

    Methods of intiializing an array

---
+++ array.js
@@ -0,0 +1,9 @@
+//Writing arrays
+
+// Using Array constructor
+let arr1 = new Array('apple', 'banana', 'cherry');
+console.log(arr1);
+
+//Using array literal notation
+let arr2 = ['apple', 'banana', 'cherry'];
+console.log(arr2);
