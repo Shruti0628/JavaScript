58ad934f018f9505141bf31718ffbfc93679e091
Shruti Rouniyar <shrutirouniyar06@gmail.com>
2025-07-27 11:13:21 +0530

    Arrays and Loops to be used in arrays along with map()

--- array.js
+++ array.js
@@ -6,4 +6,15 @@ console.log(arr1);
 
 //Using array literal notation
 let arr2 = ['apple', 'banana', 'cherry'];
-console.log(arr2);
+console.log(arr2); 
+
+//Using forEach()
+arr1.forEach((currEle, index,arr)=>{
+console.log (`This is the ${currEle} at ${index},and these are the complete list [${arr}]`);
+})
+
+//Using map()
+const myArr = arr2.map((currEle, index, arr) => {
+   return `This is the ${currEle} at ${index}, and these are the complete list [${arr}]`;
+})
+console.log(myArr);
