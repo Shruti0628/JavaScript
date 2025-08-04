535bf1dfb28231cb4dcd5ce570205c8788e0323a
Shruti Rouniyar <shrutirouniyar06@gmail.com>
2025-08-04 21:36:30 +0530

    Challenges on Array

---
+++ todo_array.js
@@ -0,0 +1,25 @@
+//Challenge time
+/*
+Ques.1) Add "Dec" at the end of an array?
+Ques.2) What is the return value of splice method?
+Ques.3) Update march to March(update)?
+Ques.4) Delete June from an array?
+*/
+
+//1. Ans -> using slice()
+const months = ['Jan','Feb','mar','Apr','June'];
+months.splice(months.length, 0, "Dec");
+console.log(months);
+
+//2. Ans-> returns an empty array [].
+// console.log(months.splice(months.length,0,"Dec"));
+
+//3. Ans-> 
+const indexToUpdate = months.indexOf("mar");
+months.splice(indexToUpdate,1,"March");
+console.log(months);
+
+//4. Ans->
+const delIndex = months.indexOf("June");
+months.splice(delIndex,1);
+console.log(months);
