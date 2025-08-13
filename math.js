1031bc4edb2cc680d71f9f022b419483ca2c11c3
Shruti Rouniyar <shrutirouniyar06@gmail.com>
2025-08-13 12:29:16 +0530

    Math Objects in JavaScript

---
+++ math.js
@@ -0,0 +1,32 @@
+//Math Object in JavaScript
+
+/* Math: The Math namespace object contains static properties and methods for mathematical
+constants and functions.
+It works with the Number type. It doesn't work with BigInt.
+*/
+
+//Constants:
+/* Math.PI = Represents the mathematical constant Pi(n).*/
+const piValue = Math.PI;
+console.log(piValue);
+
+// Math.abs(): The static method returns the absolute value of a number. [Reality is: how far is it from 0]
+// Math.round(x):Rounds a number to the nearest integer.
+// Math.ceil(x): Returns the value of x rounded up to its nearest integer.
+// Math.floor(x): Returns the value of x rounded down to its nearest integer.
+// Math.trunc(x): Returns the integer part of x.
+
+/* 
+        -----Note-----
+    {Math.floor() and Math.trunc() works the same but only performs different when there is negative sign.}
+*/
+
+//-----Exponential and Logarithmic Functions-----
+// Math.pow(x,y): Returns the value of x to the power of y.
+// Math.sqrt(): It returns the square root of x.
+// Math.log(x): It returns the natural logarithmic of x.
+// Math.log2(x): It returns the base 2 logarithmic of x.
+// Math.random(): It returns a random number between 0 (inclusive), and 1 (exclusive).
+
+
+
