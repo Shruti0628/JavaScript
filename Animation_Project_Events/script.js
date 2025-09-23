0044a7cedf53b8cb7377cdf77b42fcc4bb03bb7d
Shruti Rouniyar <shrutirouniyar06@gmail.com>
2025-09-23 13:13:13 +0530

    Project for Simple Understanding of Events

---
+++ Animation_Project_Events/script.js
@@ -0,0 +1,16 @@
+let myButton = document.getElementById("myButton");
+let rect = document.querySelector(".shape_rectangle");
+
+function handleMouseEvents() {
+    console.log("click");
+    rect.style.backgroundColor = "yellow";
+};
+
+function handleMouseEvents2() {
+    console.log("double-click");
+    rect.style.backgroundColor = "orange";
+    rect.classList.add("addTransition"); //to add the transition effect on double click
+};
+
+myButton.addEventListener("click", handleMouseEvents); //since, no args are passed, we can use directly a function rather than a callback function
+myButton.addEventListener("dblclick",handleMouseEvents2);
