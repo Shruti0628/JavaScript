3cf959c4e16a3dd0ae6a5d7e3877f453903c50e0
Shruti Rouniyar <shrutirouniyar06@gmail.com>
2025-09-27 22:06:43 +0530

    Concepts with examples of KeyBoard Events

--- Keyboard_Events/script.js
+++ Keyboard_Events/script.js
@@ -5,5 +5,20 @@ const handleKeyPress = ( event )=>{
     console.log("Key Pressed: " , event.key);
     console.log("Key Code: ", event.code);
     console.log("Event Type: ", event.type);
+
+    // Displaying the values in the HTML by targeting the classes
+    document.querySelector(
+        ".keyPressed"   
+    ).textContent = `Key Pressed: ${event.key}`;
+    document.querySelector(
+        ".keyCode"
+    ).textContent = `Key Code: ${event.code}`;
+    document.querySelector(
+        ".charCode"
+    ).textContent = `Char Code: ${event.key.charCodeAt(0)}`;
+    document.querySelector(
+        ".eventType"
+    ).textContent = `Event Type: ${event.type}`;
+
 }
 myInput.addEventListener("keydown", handleKeyPress);
